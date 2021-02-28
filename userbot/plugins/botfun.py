44f9cbd8c3e3f64859dee9a50468db2a4fc0971e
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-02-28 15:13:46 +0530

    Update botfun.py

--- userbot/plugins/botfun.py
+++ userbot/plugins/botfun.py
@@ -125,19 +125,6 @@ async def _(event):
         )
 
 
-@borg.on(admin_cmd(pattern="stat$"))
-async def stats(event):
-    if event.fwd_from:
-        return
-    botusername = Var.TG_BOT_USER_NAME_BF_HER
-    noob = "stats"
-    if event.reply_to_msg_id:
-        await event.get_reply_message()
-    tap = await bot.inline_query(botusername, noob)
-    await tap[0].click(event.chat_id)
-    await event.delete()
-
-
 @borg.on(admin_cmd(pattern="xogame$"))
 async def gamez(event):
     if event.fwd_from:
