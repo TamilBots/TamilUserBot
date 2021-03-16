75e1f061fd0100f53edcb8400f5db482a38b8323
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-03-16 15:51:59 +0530

    Update utils.py

--- userbot/utils.py
+++ userbot/utils.py
@@ -168,7 +168,8 @@ def admin_cmd(pattern=None, **args):
         del args["allow_edited_updates"]
 
     # check if the plugin should listen for outgoing 'messages'
-    is_message_enabled = True
+
+    return events.NewMessage(**args)
 
 async def edit_or_reply(event, text):
     if event.sender_id in Config.SUDO_USERS:
