1419a2116866001a36b568c0f34c0651433c6f3f
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-03-16 11:39:05 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -142,7 +142,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
         if event.query.user_id == bot.uid:
             text = inlinestats
             await event.answer(text, alert=True)
-       else:
+        else:
             txt = "You Can't View My Masters Stats"
             await event.answer(txt, alert=True)
         
