c6dd4a5cd2529a2f5224a11ef19bd021cf5e4eee
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-03-16 16:34:29 +0530

    Update broadcast_sql.py

--- userbot/plugins/sql_helper/broadcast_sql.py
+++ userbot/plugins/sql_helper/broadcast_sql.py
@@ -1,142 +1,45 @@
 # modified by @saravanakrish for @tamiluserbot
 # Credits : @HeisenbergTheDanger And Friday Userbot
 
-import threading
-
-from sqlalchemy import Column, String, UnicodeText, distinct, func
+from sqlalchemy import Column, String
 
 from userbot.plugins.sql_helper import BASE, SESSION
 
 
-class CatBroadcast(BASE):
-    __tablename__ = "catbroadcast"
-    keywoard = Column(UnicodeText, primary_key=True)
-    group_id = Column(String(14), primary_key=True, nullable=False)
+class ghdb(BASE):
+    __tablename__ = "channels"
+    chat_id = Column(String(14), primary_key=True)
 
-    def __init__(self, keywoard, group_id):
-        self.keywoard = keywoard
-        self.group_id = str(group_id)
-
-    def __repr__(self):
-        return "<Cat Broadcast channels '%s' for %s>" % (self.group_id, self.keywoard)
-
-    def __eq__(self, other):
-        return bool(
-            isinstance(other, CatBroadcast)
-            and self.keywoard == other.keywoard
-            and self.group_id == other.group_id
-        )
+    def __init__(self, chat_id):
+        self.chat_id = chat_id
 
 
-CatBroadcast.__table__.create(checkfirst=True)
-
-CATBROADCAST_INSERTION_LOCK = threading.RLock()
+ghdb.__table__.create(checkfirst=True)
 
 
-class BROADCAST_SQL:
-    def __init__(self):
-        self.BROADCAST_CHANNELS = {}
-
-
-BROADCAST_SQL_ = BROADCAST_SQL()
-
-
-def add_to_broadcastlist(keywoard, group_id):
-    with CATBROADCAST_INSERTION_LOCK:
-        broadcast_group = CatBroadcast(keywoard, str(group_id))
-
-        SESSION.merge(broadcast_group)
-        SESSION.commit()
-        BROADCAST_SQL_.BROADCAST_CHANNELS.setdefault(keywoard, set()).add(str(group_id))
-
-
-def rm_from_broadcastlist(keywoard, group_id):
-    with CATBROADCAST_INSERTION_LOCK:
-        broadcast_group = SESSION.query(CatBroadcast).get((keywoard, str(group_id)))
-        if broadcast_group:
-            if str(group_id) in BROADCAST_SQL_.BROADCAST_CHANNELS.get(keywoard, set()):
-                BROADCAST_SQL_.BROADCAST_CHANNELS.get(keywoard, set()).remove(
-                    str(group_id)
-                )
-
-            SESSION.delete(broadcast_group)
-            SESSION.commit()
-            return True
-
+def in_channels(chat_id):
+    try:
+        return SESSION.query(ghdb).filter(ghdb.chat_id == str(chat_id)).one()
+    except:
+        return None
+    finally:
         SESSION.close()
-        return False
 
 
-def is_in_broadcastlist(keywoard, group_id):
-    with CATBROADCAST_INSERTION_LOCK:
-        broadcast_group = SESSION.query(CatBroadcast).get((keywoard, str(group_id)))
-        return bool(broadcast_group)
+def add_channel(chat_id):
+    adder = ghdb(str(chat_id))
+    SESSION.add(adder)
+    SESSION.commit()
 
 
-def del_keyword_broadcastlist(keywoard):
-    with CATBROADCAST_INSERTION_LOCK:
-        broadcast_group = (
-            SESSION.query(CatBroadcast.keywoard)
-            .filter(CatBroadcast.keywoard == keywoard)
-            .delete()
-        )
-        BROADCAST_SQL_.BROADCAST_CHANNELS.pop(keywoard)
+def rm_channel(chat_id):
+    rem = SESSION.query(ghdb).get(str(chat_id))
+    if rem:
+        SESSION.delete(rem)
         SESSION.commit()
 
 
-def get_chat_broadcastlist(keywoard):
-    return BROADCAST_SQL_.BROADCAST_CHANNELS.get(keywoard, set())
-
-
-def get_broadcastlist_chats():
-    try:
-        chats = SESSION.query(CatBroadcast.keywoard).distinct().all()
-        return [i[0] for i in chats]
-    finally:
-        SESSION.close()
-
-
-def num_broadcastlist():
-    try:
-        return SESSION.query(CatBroadcast).count()
-    finally:
-        SESSION.close()
-
-
-def num_broadcastlist_chat(keywoard):
-    try:
-        return (
-            SESSION.query(CatBroadcast.keywoard)
-            .filter(CatBroadcast.keywoard == keywoard)
-            .count()
-        )
-    finally:
-        SESSION.close()
-
-
-def num_broadcastlist_chats():
-    try:
-        return SESSION.query(func.count(distinct(CatBroadcast.keywoard))).scalar()
-    finally:
-        SESSION.close()
-
-
-def __load_chat_broadcastlists():
-    try:
-        chats = SESSION.query(CatBroadcast.keywoard).distinct().all()
-        for (keywoard,) in chats:
-            BROADCAST_SQL_.BROADCAST_CHANNELS[keywoard] = []
-
-        all_groups = SESSION.query(CatBroadcast).all()
-        for x in all_groups:
-            BROADCAST_SQL_.BROADCAST_CHANNELS[x.keywoard] += [x.group_id]
-
-        BROADCAST_SQL_.BROADCAST_CHANNELS = {
-            x: set(y) for x, y in BROADCAST_SQL_.BROADCAST_CHANNELS.items()
-        }
-
-    finally:
-        SESSION.close()
-
-
-__load_chat_broadcastlists()
+def get_all_channels():
+    rem = SESSION.query(ghdb).all()
+    SESSION.close()
+    return rem
