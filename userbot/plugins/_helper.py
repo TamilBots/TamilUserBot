3d2c9e9c4eedafaa2d8eb6fe18d4a56a124e6950
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-03-18 00:55:15 +0530

    Update _helper.py

--- userbot/plugins/_helper.py
+++ userbot/plugins/_helper.py
@@ -42,7 +42,7 @@ async def cmd_list(event):
             else:
                 await event.edit(input_str + " is not a valid plugin!")
         else:
-            help_string = """`Bot.`"""
+            help_string = """`Bot of.`"""
             results = await bot.inline_query(  # pylint:disable=E0602
                 tgbotusername,
                 help_string
