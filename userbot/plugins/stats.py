3ddd33f820d25a8e636a94795146e008e704133e
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-03-18 21:56:16 +0530

    Update stats.py

--- userbot/plugins/stats.py
+++ userbot/plugins/stats.py
@@ -76,17 +76,17 @@ async def stats(
         unread += dialog.unread_count
     stop_time = time.time() - start_time
     full_name = inline_mention(await event.client.get_me())
-    response = f"📌 **Stats for {full_name}** \n\n"
+    response = f"🔹 **Stats 🔹for {full_name}** \n\n"
     response += f"**Private Chats:** {private_chats} \n"
-    response += f"   ★ `Users: {private_chats - bots}` \n"
-    response += f"   ★ `Bots: {bots}` \n"
+    response += f"   🔸 `Users: {private_chats - bots}` \n"
+    response += f"   🔸 `Bots: {bots}` \n"
     response += f"**Groups:** {groups} \n"
     response += f"**Channels:** {broadcast_channels} \n"
     response += f"**Admin in Groups:** {admin_in_groups} \n"
-    response += f"   ★ `Creator: {creator_in_groups}` \n"
-    response += f"   ★ `Admin Rights: {admin_in_groups - creator_in_groups}` \n"
+    response += f"   🔸 `Creator: {creator_in_groups}` \n"
+    response += f"   🔸 `Admin Rights: {admin_in_groups - creator_in_groups}` \n"
     response += f"**Admin in Channels:** {admin_in_broadcast_channels} \n"
-    response += f"   ★ `Creator: {creator_in_channels}` \n"
+    response += f"   🔸 `Creator: {creator_in_channels}` \n"
     response += (
         f"   ★ `Admin Rights: {admin_in_broadcast_channels - creator_in_channels}` \n"
     )
