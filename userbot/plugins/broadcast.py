a630522e01031c6e5d29ca1b6d913a3c0195ada4
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-03-18 21:21:28 +0530

    Update broadcast.py

--- userbot/plugins/broadcast.py
+++ userbot/plugins/broadcast.py
@@ -21,7 +21,7 @@ async def catbroadcast_send(event):
         return
     catinput_str = event.pattern_match.group(1)
     if not catinput_str:
-        return await edit_delete(
+        return await event.edit(
             event, "To which category should i send this message", parse_mode=parse_pre
         )
     reply = await event.get_reply_message()
@@ -129,19 +129,19 @@ async def catbroadcast_add(event):
         return
     catinput_str = event.pattern_match.group(1)
     if not catinput_str:
-        return await edit_delete(
+        return await event.edit(
             event, "In which category should i add this chat", parse_mode=parse_pre
         )
     keyword = catinput_str.lower()
     check = sql.is_in_broadcastlist(keyword, event.chat_id)
     if check:
-        return await edit_delete(
+        return await event.edit(
             event,
             f"This chat is already in this category {keyword}",
             parse_mode=parse_pre,
         )
     sql.add_to_broadcastlist(keyword, event.chat_id)
-    await edit_delete(
+    await event.edit(
         event, f"This chat is Now added to category {keyword}", parse_mode=parse_pre
     )
     chat = await event.get_chat()
