ab5e3cbd0feff333fc55d893b0da071139bfb9f1
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-04-01 23:37:26 +0530

    Update stickertools.py

--- userbot/plugins/stickertools.py
+++ userbot/plugins/stickertools.py
@@ -448,79 +448,35 @@ async def waifu(animu):
 # 12 21 28 30
 
 
-@borg.on(admin_cmd(pattern=r"stcr ?(?:(.*?) \| )?(.*)", outgoing=True))
-async def sticklet(event):
-    R = random.randint(0, 256)
-    G = random.randint(0, 256)
-    B = random.randint(0, 256)
-    reply_message = event.message
-    # get the input text
-    # the text on which we would like to do the magic on
-    font_file_name = event.pattern_match.group(1)
-    if not font_file_name:
-        font_file_name = ""
-    sticktext = event.pattern_match.group(2)
-    if not sticktext and event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-        sticktext = reply_message.message
-    elif not sticktext:
-        await edit_or_reply(event, "need something, hmm")
-        return
-    if event.reply_to_msg_id:
-        reply_message = await event.get_reply_message()
-    # delete the userbot command,
-    # i don't know why this is required
-    await event.delete()
-    sticktext = deEmojify(sticktext)
-    # https://docs.python.org/3/library/textwrap.html#textwrap.wrap
-    sticktext = textwrap.wrap(sticktext, width=10)
-    # converts back the list to a string
-    sticktext = "\n".join(sticktext)
-    image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
-    draw = ImageDraw.Draw(image)
-    fontsize = 230
-    FONT_FILE = await get_font_file(event.client, "@catfonts", font_file_name)
-    font = ImageFont.truetype(FONT_FILE, size=fontsize)
-    while draw.multiline_textsize(sticktext, font=font) > (512, 512):
-        fontsize -= 3
-        font = ImageFont.truetype(FONT_FILE, size=fontsize)
-    width, height = draw.multiline_textsize(sticktext, font=font)
-    draw.multiline_text(
-        ((512 - width) / 2, (512 - height) / 2), sticktext, font=font, fill=(R, G, B)
-    )
-    image_stream = io.BytesIO()
-    image_stream.name = "@catuserbot17.webp"
-    image.save(image_stream, "WebP")
-    image_stream.seek(0)
-    # finally, reply the sticker
-    await event.client.send_file(
-        event.chat_id,
-        image_stream,
-        caption="cat's Sticklet",
-        reply_to=event.message.reply_to_msg_id,
-    )
-    # cleanup
+@borg.on(admin_cmd(pattern=r"stcr ?(.*)", outgoing=True))
+async def waifu(animu):
+    xx = await eor(animu, "`Processing...`")
+    # """Creates random anime sticker!"""
+    text = animu.pattern_match.group(1)
+    if not text:
+        if animu.is_reply:
+            text = (await animu.get_reply_message()).message
+        else:
+            await xx.edit("`You haven't written any article, Waifu is going away.`")
+            return
+    waifus = [32, 33, 37, 40, 41, 42, 58, 20]
+    finalcall = "#" + (str(random.choice(waifus)))
     try:
-        os.remove(FONT_FILE)
-    except BaseException:
-        pass
+        sticcers = await ultroid_bot.inline_query(
+            "stickerizerbot", f"{finalcall}{(deEmojify(text))}"
+        )
+        await sticcers[0].click(
+            animu.chat_id,
+            reply_to=animu.reply_to_msg_id,
+            silent=True if animu.is_reply else False,
+            hide_via=True,
+        )
+        await xx.delete()
+    except ChatSendInlineForbiddenError:
+        await xx.edit("`Boss ! I cant use inline things here...`")
+    except ChatSendStickersForbiddenError:
+        await xx.edit("Sorry boss, I can't send Sticker Here !!")
 
-
-async def get_font_file(client, channel_id, search_kw=""):
-    # first get the font messages
-    font_file_message_s = await client.get_messages(
-        entity=channel_id,
-        filter=InputMessagesFilterDocument,
-        # this might cause FLOOD WAIT,
-        # if used too many times
-        limit=None,
-        search=search_kw,
-    )
-    # get a random font from the list of fonts
-    # https://docs.python.org/3/library/random.html#random.choice
-    font_file_message = random.choice(font_file_message_s)
-    # download and return the file path
-    return await client.download_media(font_file_message)
 
 
 CMD_HELP.update(
@@ -537,7 +493,7 @@ CMD_HELP.update(
 \n\n**Syntax : **`.sttxt` <your txt>\
 \n**Usage : **Anime that makes your writing fun.\
 \n\n**Syntax : **`.stcr` <your txt>\
-\n**Usage : **your text as sticker\
+\n**Usage : ** paste text on random stickers.\
 \n\n**Syntax : **`.stkrinfo`\
 \n**Usage : **Gets info about the sticker pack."
     }
