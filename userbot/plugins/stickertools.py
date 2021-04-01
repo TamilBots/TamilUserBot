265afc1a04a92b270d0f607ddca9b4bee3c93997
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-04-01 21:43:18 +0530

    Update stickertools.py

--- userbot/plugins/stickertools.py
+++ userbot/plugins/stickertools.py
@@ -10,11 +10,12 @@ import base64
 import json
 import telethon
 import emoji
+import textwrap
 import urllib.request
 from os import remove
 from io import BytesIO
 
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.functions.messages import GetStickerSetRequest
 from telethon.tl.types import (
     DocumentAttributeFilename,
@@ -24,7 +25,7 @@ from telethon.tl.types import (
 )
 
 from userbot import CMD_HELP, bot
-from userbot.utils import admin_cmd
+from userbot.utils import admin_cmd, edit_or_reply, sudo_cmd
 
 
 logger = logging.getLogger(__name__)
@@ -357,199 +358,103 @@ async def get_pack_info(event):
     )
     await event.edit(OUTPUT)
 
-if 1 == 1:
-    strings = {
-        "name": "Quotes",
-        "api_token_cfg_doc": "API Key/Token for Quotes.",
-        "api_url_cfg_doc": "API URL for Quotes.",
-        "colors_cfg_doc": "Username colors",
-        "default_username_color_cfg_doc": "Default color for the username.",
-        "no_reply": "<b>You didn't reply to a message.</b>",
-        "no_template": "<b>You didn't specify the template.</b>",
-        "delimiter": "</code>, <code>",
-        "server_error": "<b>Server error. Please report to developer.</b>",
-        "invalid_token": "<b>You've set an invalid token.</b>",
-        "unauthorized": "<b>You're unauthorized to do this.</b>",
-        "not_enough_permissions": "<b>Wrong template. You can use only the default one.</b>",
-        "templates": "<b>Available Templates:</b> <code>{}</code>",
-        "cannot_send_stickers": "<b>You cannot send stickers in this chat.</b>",
-        "admin": "admin",
-        "creator": "creator",
-        "hidden": "hidden",
-        "channel": "Channel"
-    }
-
-    config = dict({"api_token": os.environ.get("API_TOKEN"), 
-                                          "api_url": "http://api.antiddos.systems",
-                                          "username_colors": ["#fb6169", "#faa357", "#b48bf2", "#85de85",
-                                                              "#62d4e3", "#65bdf3", "#ff5694"],
-                                          "default_username_color": "#b48bf2"})
-    client = borg
-
-    @borg.on(admin_cmd(pattern="chat(.*)"))
-    async def quotecmd(message):  # noqa: C901
-        """Quote a message.
-        Usage: .quote [template]
-        If template is missing, possible templates are fetched."""
-        await message.delete()
-        args = message.raw_text.split(" ")[1:]
-        if args == []:
-            args = ["default"]
-        reply = await message.get_reply_message()
-
-        if not reply:
-            return await message.respond(strings["no_reply"])
-
-        if not args:
-            return await message.respond(strings["no_template"])
-
-        username_color = username = admintitle = user_id = None
-        profile_photo_url = reply.from_id
-
-        admintitle = ""
-        if isinstance(message.to_id, telethon.tl.types.PeerChannel):
-            try:
-                user = await client(telethon.tl.functions.channels.GetParticipantRequest(message.chat_id,
-                                                                                              reply.from_id))
-                if isinstance(user.participant, telethon.tl.types.ChannelParticipantCreator):
-                    admintitle = user.participant.rank or strings["creator"]
-                elif isinstance(user.participant, telethon.tl.types.ChannelParticipantAdmin):
-                    admintitle = user.participant.rank or strings["admin"]
-                user = user.users[0]
-            except telethon.errors.rpcerrorlist.UserNotParticipantError:
-                user = await reply.get_sender()
-        elif isinstance(message.to_id, telethon.tl.types.PeerChat):
-            chat = await client(telethon.tl.functions.messages.GetFullChatRequest(reply.to_id))
-            participants = chat.full_chat.participants.participants
-            participant = next(filter(lambda x: x.user_id == reply.from_id, participants), None)
-            if isinstance(participant, telethon.tl.types.ChatParticipantCreator):
-                admintitle = strings["creator"]
-            elif isinstance(participant, telethon.tl.types.ChatParticipantAdmin):
-                admintitle = strings["admin"]
-            user = await reply.get_sender()
+@borg.on(admin_cmd(outgoing=True, pattern="sttxt(?: |$)(.*)"))
+@borg.on(sudo_cmd(allow_sudo=True, pattern="sttxt(?: |$)(.*)"))
+async def waifu(animu):
+    text = animu.pattern_match.group(1)
+    reply_to_id = animu.message
+    if animu.reply_to_msg_id:
+        reply_to_id = await animu.get_reply_message()
+    if not text:
+        if animu.is_reply:
+            text = (await animu.get_reply_message()).message
         else:
-            user = await reply.get_sender()
-
-        username = telethon.utils.get_display_name(user)
-        user_id = reply.from_id
-
-        if reply.fwd_from:
-            if reply.fwd_from.saved_from_peer:
-                username = telethon.utils.get_display_name(reply.forward.chat)
-                profile_photo_url = reply.forward.chat
-                admintitle = strings["channel"]
-            elif reply.fwd_from.from_name:
-                username = reply.fwd_from.from_name
-            elif reply.forward.sender:
-                username = telethon.utils.get_display_name(reply.forward.sender)
-            elif reply.forward.chat:
-                username = telethon.utils.get_display_name(reply.forward.chat)
-
-        pfp = await client.download_profile_photo(profile_photo_url, bytes)
-        if pfp is not None:
-            profile_photo_url = "data:image/png;base64, " + base64.b64encode(pfp).decode()
-
-        if user_id is not None:
-            username_color = config["username_colors"][user_id % 7]
-        else:
-            username_color = config["default_username_color"]
-
-        request = json.dumps({
-            "ProfilePhotoURL": profile_photo_url,
-            "usernameColor": username_color,
-            "username": username,
-            "adminTitle": admintitle,
-            "Text": reply.message,
-            "Markdown": get_markdown(reply),
-            "Template": args[0],
-            "APIKey": config["api_token"]
-        })
-
-        resp = requests.post(config["api_url"] + "/api/v2/quote", data=request)
-        resp.raise_for_status()
-        resp = resp.json()
-
-        if resp["status"] == 500:
-            return await message.respond(strings["server_error"])
-        elif resp["status"] == 401:
-            if resp["message"] == "ERROR_TOKEN_INVALID":
-                return await message.respond(strings["invalid_token"])
-            else:
-                raise ValueError("Invalid response from server", resp)
-        elif resp["status"] == 403:
-            if resp["message"] == "ERROR_UNAUTHORIZED":
-                return await message.respond(strings["unauthorized"])
-            else:
-                raise ValueError("Invalid response from server", resp)
-        elif resp["status"] == 404:
-            if resp["message"] == "ERROR_TEMPLATE_NOT_FOUND":
-                newreq = requests.post(config["api_url"] + "/api/v1/getalltemplates", data={
-                    "token": config["api_token"]
-                })
-                newreq = newreq.json()
-
-                if newreq["status"] == "NOT_ENOUGH_PERMISSIONS":
-                    return await message.respond(strings["not_enough_permissions"])
-                elif newreq["status"] == "SUCCESS":
-                    templates = strings["delimiter"].join(newreq["message"])
-                    return await message.respond(strings["templates"].format(templates))
-                elif newreq["status"] == "INVALID_TOKEN":
-                    return await message.respond(strings["invalid_token"])
-                else:
-                    raise ValueError("Invalid response from server", newreq)
-            else:
-                raise ValueError("Invalid response from server", resp)
-        elif resp["status"] != 200:
-            raise ValueError("Invalid response from server", resp)
-
-        req = requests.get(config["api_url"] + "/cdn/" + resp["message"])
-        req.raise_for_status()
-        file = BytesIO(req.content)
-        file.seek(0)
-
-        img = Image.open(file)
-        with BytesIO() as sticker:
-            img.save(sticker, "webp")
-            sticker.name = "sticker.webp"
-            sticker.seek(0)
-            try:
-                await reply.reply(file=sticker)
-            except telethon.errors.rpcerrorlist.ChatSendStickersForbiddenError:
-                await message.respond(strings["cannot_send_stickers"])
-            file.close()
+            await edit_or_reply(
+                animu, "`You haven't written any article, Waifu is going away.`"
+            )
+            return
+    text = deEmojify(text)
+    await animu.delete()
+    await waifutxt(text, animu.chat_id, reply_to_id, bot, borg)
 
 
-def get_markdown(reply):
-    if not reply.entities:
-        return []
+# 12 21 28 30
 
-    markdown = []
-    for entity in reply.entities:
-        md_item = {
-            "Type": None,
-            "Start": entity.offset,
-            "End": entity.offset + entity.length - 1
-        }
-        if isinstance(entity, telethon.tl.types.MessageEntityBold):
-            md_item["Type"] = "bold"
-        elif isinstance(entity, telethon.tl.types.MessageEntityItalic):
-            md_item["Type"] = "italic"
-        elif isinstance(entity, (telethon.tl.types.MessageEntityMention, telethon.tl.types.MessageEntityTextUrl,
-                                 telethon.tl.types.MessageEntityMentionName, telethon.tl.types.MessageEntityHashtag,
-                                 telethon.tl.types.MessageEntityCashtag, telethon.tl.types.MessageEntityBotCommand,
-                                 telethon.tl.types.MessageEntityUrl)):
-            md_item["Type"] = "link"
-        elif isinstance(entity, telethon.tl.types.MessageEntityCode):
-            md_item["Type"] = "code"
-        elif isinstance(entity, telethon.tl.types.MessageEntityStrike):
-            md_item["Type"] = "stroke"
-        elif isinstance(entity, telethon.tl.types.MessageEntityUnderline):
-            md_item["Type"] = "underline"
-        else:
-            logger.warning("Unknown entity: " + str(entity))
 
-        markdown.append(md_item)
-    return markdown
+@borg.on(admin_cmd(pattern=r"stcr ?(?:(.*?) \| )?(.*)", outgoing=True))
+@borg.on(sudo_cmd(pattern=r"stcr ?(?:(.*?) \| )?(.*)", allow_sudo=True))
+async def sticklet(event):
+    R = random.randint(0, 256)
+    G = random.randint(0, 256)
+    B = random.randint(0, 256)
+    reply_message = event.message
+    # get the input text
+    # the text on which we would like to do the magic on
+    font_file_name = event.pattern_match.group(1)
+    if not font_file_name:
+        font_file_name = ""
+    sticktext = event.pattern_match.group(2)
+    if not sticktext and event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        sticktext = reply_message.message
+    elif not sticktext:
+        await edit_or_reply(event, "need something, hmm")
+        return
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+    # delete the userbot command,
+    # i don't know why this is required
+    await event.delete()
+    sticktext = deEmojify(sticktext)
+    # https://docs.python.org/3/library/textwrap.html#textwrap.wrap
+    sticktext = textwrap.wrap(sticktext, width=10)
+    # converts back the list to a string
+    sticktext = "\n".join(sticktext)
+    image = Image.new("RGBA", (512, 512), (255, 255, 255, 0))
+    draw = ImageDraw.Draw(image)
+    fontsize = 230
+    FONT_FILE = await get_font_file(event.client, "@catfonts", font_file_name)
+    font = ImageFont.truetype(FONT_FILE, size=fontsize)
+    while draw.multiline_textsize(sticktext, font=font) > (512, 512):
+        fontsize -= 3
+        font = ImageFont.truetype(FONT_FILE, size=fontsize)
+    width, height = draw.multiline_textsize(sticktext, font=font)
+    draw.multiline_text(
+        ((512 - width) / 2, (512 - height) / 2), sticktext, font=font, fill=(R, G, B)
+    )
+    image_stream = io.BytesIO()
+    image_stream.name = "@catuserbot17.webp"
+    image.save(image_stream, "WebP")
+    image_stream.seek(0)
+    # finally, reply the sticker
+    await event.client.send_file(
+        event.chat_id,
+        image_stream,
+        caption="cat's Sticklet",
+        reply_to=event.message.reply_to_msg_id,
+    )
+    # cleanup
+    try:
+        os.remove(FONT_FILE)
+    except BaseException:
+        pass
+
+
+async def get_font_file(client, channel_id, search_kw=""):
+    # first get the font messages
+    font_file_message_s = await client.get_messages(
+        entity=channel_id,
+        filter=InputMessagesFilterDocument,
+        # this might cause FLOOD WAIT,
+        # if used too many times
+        limit=None,
+        search=search_kw,
+    )
+    # get a random font from the list of fonts
+    # https://docs.python.org/3/library/random.html#random.choice
+    font_file_message = random.choice(font_file_message_s)
+    # download and return the file path
+    return await client.download_media(font_file_message)
 
 
 CMD_HELP.update(
@@ -563,6 +468,10 @@ CMD_HELP.update(
 \n**Usage : **Kang's the sticker/image to the specified pack but uses 🤔 as emoji.\
 \n\n**Syntax : **`.kang [emoji('s)] [number]`\
 \n**Usage : **Kang's the sticker/image to the specified pack and uses the emoji('s) you picked.\
+\n\n**Syntax : **`.sttxt` <your txt>\
+\n**Usage : **Anime that makes your writing fun.\
+\n\n**Syntax : **`.stcr` <your txt>\
+\n**Usage : **your text as sticker\
 \n\n**Syntax : **`.stkrinfo`\
 \n**Usage : **Gets info about the sticker pack."
     }
