e893306131640d89dd55c7dab50aa14973f4ad34
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-04-02 20:36:59 +0530

    Update stickertools.py

--- userbot/plugins/stickertools.py
+++ userbot/plugins/stickertools.py
@@ -4,7 +4,10 @@ import os
 import re
 import time
 import zipfile
+import asyncio
 from random import choice
+import datetime
+from collections import defaultdict
 import io
 import math
 import random
@@ -19,12 +22,23 @@ import urllib.request
 from os import remove
 from io import BytesIO
 import PIL.ImageOps
+from datetime import datetime
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from hachoir.metadata import extractMetadata
+from hachoir.parser import createParser
+from pySmartDL import SmartDL
 
 from PIL import Image, ImageDraw, ImageFont
 from telethon.tl.functions.messages import GetStickerSetRequest
 from telethon.tl.types import (
     DocumentAttributeFilename,
     DocumentAttributeSticker,
+    DocumentAttributeVideo,
+    InputMediaUploadedDocument,
+    InputPeerNotifySettings,
+    InputStickerSetShortName,
     InputStickerSetID,
     MessageMediaPhoto,
 )
@@ -32,8 +46,13 @@ from telethon.tl.types import (
 from telethon.errors import (ChatSendInlineForbiddenError,
                              ChatSendStickersForbiddenError)
 
+from telethon.errors.rpcerrorlist import StickersetInvalidError
+from telethon.errors import MessageNotModifiedError
+from telethon.tl.functions.account import UpdateNotifySettingsRequest
+from telethon.tl.functions.messages import GetStickerSetRequest
+
 from userbot import CMD_HELP, bot
-from userbot.utils import admin_cmd, sudo_cmd
+from userbot.utils import admin_cmd, sudo_cmd, progress, humanbytes, time_formatter
 from userbot.manager.utils import edit_or_reply
 
 logger = logging.getLogger(__name__)
@@ -41,7 +60,8 @@ logger = logging.getLogger(__name__)
 from telethon.tl.types import Channel, PollAnswer
 from validators.url import url
 
-eor = edit_or_reply
+
+thumb_image_path = Config.TMP_DOWNLOAD_DIRECTORY + "/thumb_image.jpg"
 
 KANGING_STR = [
     "Using Witchery to kang this sticker...",
@@ -452,35 +472,109 @@ async def waifu(animu):
 
 # 12 21 28 30
 
-
-@borg.on(admin_cmd(pattern=r"stcr ?(.*)", outgoing=True))
-async def waifu(animu):
-    xx = await eor(animu, "`Processing...`")
-    # """Creates random anime sticker!"""
-    text = animu.pattern_match.group(1)
-    if not text:
-        if animu.is_reply:
-            text = (await animu.get_reply_message()).message
-        else:
-            await xx.edit("`You haven't written any article, Waifu is going away.`")
+@borg.on(admin_cmd("mmf ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return 
+    if not event.reply_to_msg_id:
+       await event.edit("`Syntax: reply to an image with .mms` 'text on top' ; 'text on bottom' ")
+       return
+    reply_message = await event.get_reply_message() 
+    if not reply_message.media:
+       await event.edit("```reply to a image/sticker/gif```")
+       return
+    chat = "@MemeAutobot"
+    sender = reply_message.sender
+    file_ext_ns_ion = "@memetime.png"
+    file = await borg.download_file(reply_message.media)
+    uploaded_gif = None
+    if reply_message.sender.bot:
+       await event.edit("```Reply to actual users message.```")
+       return
+    else:
+     await event.edit("```Transfiguration Time! Mwahaha memifying this image! (」ﾟﾛﾟ)｣ ```")
+    
+    async with borg.conversation("@MemeAutobot") as bot_conv:
+          try:
+            memeVar = event.pattern_match.group(1)
+            await silently_send_message(bot_conv, "/start")
+            await asyncio.sleep(1)
+            await silently_send_message(bot_conv, memeVar)
+            await borg.send_file(chat, reply_message.media)
+            response = await bot_conv.get_response()
+          except YouBlockedUserError: 
+              await event.reply("```Please unblock @MemeAutobot and try again```")
+              return
+          if response.text.startswith("Forward"):
+              await event.edit("```can you kindly disable your forward privacy settings for good nibba?```")
+          if "Okay..." in response.text:
+            await event.edit("```🤨 NANI?! This is not an image! This will take sum tym to convert to image owo 🧐```")
+            thumb = None
+            if os.path.exists(thumb_image_path):
+                thumb = thumb_image_path
+            input_str = event.pattern_match.group(1)
+            if not os.path.isdir(Config.TMP_DOWNLOAD_DIRECTORY):
+                os.makedirs(Config.TMP_DOWNLOAD_DIRECTORY)
+            if event.reply_to_msg_id:
+                file_name = "meme.png"
+                reply_message = await event.get_reply_message()
+                to_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
+                downloaded_file_name = os.path.join(to_download_directory, file_name)
+                downloaded_file_name = await borg.download_media(
+                    reply_message,
+                    downloaded_file_name,
+                    )
+                if os.path.exists(downloaded_file_name):
+                    await borg.send_file(
+                        chat,
+                        downloaded_file_name,
+                        force_document=False,
+                        supports_streaming=False,
+                        allow_cache=False,
+                        thumb=thumb,
+                        )
+                    os.remove(downloaded_file_name)
+                else:
+                    await event.edit("File Not Found {}".format(input_str))
+            response = await bot_conv.get_response()
+            the_download_directory = Config.TMP_DOWNLOAD_DIRECTORY
+            files_name = "memes.webp"
+            download_file_name = os.path.join(the_download_directory, files_name)
+            await borg.download_media(
+                response.media,
+                download_file_name,
+                )
+            requires_file_name = Config.TMP_DOWNLOAD_DIRECTORY + "memes.webp"
+            await borg.send_file(  # pylint:disable=E0602
+                event.chat_id,
+                requires_file_name,
+                supports_streaming=False,
+                caption="Userbot: Powered by @JayuBot",
+                # Courtesy: @A_Dark_Princ3
+            )
+            await event.delete()
+            await borg.send_message(event.chat_id, "`☠️☠️23 Points to Griffindor!🔥🔥`")
+          elif not is_message_image(reply_message):
+            await event.edit("Invalid message type. Plz choose right message type u NIBBA.")
             return
-    waifus = [32, 33, 37, 40, 41, 42, 58, 20]
-    finalcall = "#" + (str(random.choice(waifus)))
-    try:
-        sticcers = await bot.inline_query(
-            "stickerizerbot", f"{finalcall}{(deEmojify(text))}"
-        )
-        await sticcers[0].click(
-            animu.chat_id,
-            reply_to=animu.reply_to_msg_id,
-            silent=True if animu.is_reply else False,
-            hide_via=True,
-        )
-        await xx.delete()
-    except ChatSendInlineForbiddenError:
-        await xx.edit("`Boss ! I cant use inline things here...`")
-    except ChatSendStickersForbiddenError:
-        await xx.edit("Sorry boss, I can't send Sticker Here !!")
+          else: 
+               await borg.send_file(event.chat_id, response.media)
+
+def is_message_image(message):
+    if message.media:
+        if isinstance(message.media, MessageMediaPhoto):
+            return True
+        if message.media.document:
+            if message.media.document.mime_type.split("/")[0] == "image":
+                return True
+        return False
+    return False
+    
+async def silently_send_message(conv, text):
+    await conv.send_message(text)
+    response = await conv.get_response()
+    await conv.mark_read(message=response)
+    return response
 
 
 
@@ -497,7 +591,7 @@ CMD_HELP.update(
 \n**Usage : **Kang's the sticker/image to the specified pack and uses the emoji('s) you picked.\
 \n\n**Syntax : **`.sttxt` <your txt>\
 \n**Usage : **Anime that makes your writing fun.\
-\n\n**Syntax : **`.stcr` <your txt>\
+\n\n**Syntax : **`.mmf` <your txt>\
 \n**Usage : ** paste text on random stickers.\
 \n\n**Syntax : **`.stkrinfo`\
 \n**Usage : **Gets info about the sticker pack."
