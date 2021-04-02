c08b50427d242c152a32ee8448308881e8d36c10
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-04-02 21:29:57 +0530

    Update chatinfo.py

--- userbot/plugins/chatinfo.py
+++ userbot/plugins/chatinfo.py
@@ -16,11 +16,129 @@ from telethon.errors import (
 )
 from telethon.tl.functions.channels import GetFullChannelRequest, GetParticipantsRequest
 from telethon.tl.functions.messages import GetFullChatRequest, GetHistoryRequest
-from telethon.tl.types import ChannelParticipantsAdmins, MessageActionChannelMigrateFrom
-from telethon.utils import get_input_location
+from telethon.tl.types import MessageActionChannelMigrateFrom
+from telethon.utils import get_input_location, pack_bot_file_id
 
 from userbot.utils import admin_cmd
+from userbot import 
+from telethon.tl.types import (
+    ChannelParticipantAdmin,
+    ChannelParticipantCreator,
+    ChannelParticipantsAdmins,
+    ChannelParticipantsBots,
+)
 
+
+
+@borg.on(admin_cmd(pattern="get_ad?(m)in ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "**Admins in this Channel**: \n"
+    should_mention_admins = False
+    reply_message = None
+    pattern_match_str = event.pattern_match.group(1)
+    if "m" in pattern_match_str:
+        should_mention_admins = True
+        if event.reply_to_msg_id:
+            reply_message = await event.get_reply_message()
+    input_str = event.pattern_match.group(2)
+    to_write_chat = await event.get_input_chat()
+    chat = None
+    if not input_str:
+        chat = to_write_chat
+    else:
+        mentions_heading = "Admins in {} channel: \n".format(input_str)
+        mentions = mentions_heading
+        try:
+            chat = await borg.get_entity(input_str)
+        except Exception as e:
+            await eor(event, str(e))
+            return None
+    try:
+        async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+            if not x.deleted:
+                if isinstance(x.participant, ChannelParticipantCreator):
+                    mentions += "\n 👑 [{}](tg://user?id={}) `{}`".format(
+                        x.first_name, x.id, x.id
+                    )
+        mentions += "\n"
+        async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+            if not x.deleted:
+                if isinstance(x.participant, ChannelParticipantAdmin):
+                    mentions += "\n ⚜️ [{}](tg://user?id={}) `{}`".format(
+                        x.first_name, x.id, x.id
+                    )
+            else:
+                mentions += "\n `{}`".format(x.id)
+    except Exception as e:
+        mentions += " " + str(e) + "\n"
+    if should_mention_admins:
+        if reply_message:
+            await reply_message.reply(mentions)
+        else:
+            await event.reply(mentions)
+        await event.delete()
+    else:
+        await eor(event, mentions)
+
+
+@borg.on(admin_cmd(pattern="get_id"))
+async def _(event):
+    if event.fwd_from:
+        return
+    if event.reply_to_msg_id:
+        await event.get_input_chat()
+        r_msg = await event.get_reply_message()
+        if r_msg.media:
+            bot_api_file_id = pack_bot_file_id(r_msg.media)
+            await eor(
+                event,
+                "Current Chat ID: `{}`\nFrom User ID: `{}`\nBot API File ID: `{}`".format(
+                    str(event.chat_id), str(r_msg.from_id), bot_api_file_id
+                ),
+            )
+        else:
+            await eor(
+                event,
+                "Current Chat ID: `{}`\nFrom User ID: `{}`".format(
+                    str(event.chat_id), str(r_msg.from_id)
+                ),
+            )
+    else:
+        await eor(event, "Current Chat ID: `{}`".format(str(event.chat_id)))
+
+
+@borg.on(admin_cmd(pattern="bots ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "**Bots in this Channel**: \n"
+    input_str = event.pattern_match.group(1)
+    to_write_chat = await event.get_input_chat()
+    chat = None
+    if not input_str:
+        chat = to_write_chat
+    else:
+        mentions = "Bots in {} channel: \n".format(input_str)
+        try:
+            chat = await borg.get_entity(input_str)
+        except Exception as e:
+            await eor(event, str(e))
+            return None
+    try:
+        async for x in borg.iter_participants(chat, filter=ChannelParticipantsBots):
+            if isinstance(x.participant, ChannelParticipantAdmin):
+                mentions += "\n ⚜️ [{}](tg://user?id={}) `{}`".format(
+                    x.first_name, x.id, x.id
+                )
+            else:
+                mentions += "\n [{}](tg://user?id={}) `{}`".format(
+                    x.first_name, x.id, x.id
+                )
+    except Exception as e:
+        mentions += " " + str(e) + "\n"
+    await eor(event, mentions)
 
 @borg.on(admin_cmd(pattern="chatinfo(?: |$)(.*)", outgoing=True))
 async def info(event):
@@ -288,3 +406,23 @@ async def fetch_info(chat, event):
     if description:
         caption += f"Description: \n<code>{description}</code>\n"
     return caption
+
+
+CMD_HELP.update(
+    {
+        "groupdata": "**Plugin : **`groupdata`\
+    \n\n•  **Syntax : **`.adminperm (username/reply)`\
+    \n•  **Function : **__Shows you the admin permissions in the group.__\
+    \n\n•  **Syntax : **`.get_admin or .admins <username of group >`\
+    \n•  **Function : **__Retrieves a list of admins in the chat.__\
+    \n\n•  **Syntax : **`.bots or .bots <username of group >`\
+    \n•  **Function : **__Retrieves a list of bots in the chat.__\
+    \n\n•  **Syntax : **`.users or .users <name of member>`\
+    \n•  **Function : **__Retrieves all (or queried) users in the chat.__\
+    \n\n•  **Syntax : **`.chatinfo or .chatinfo <username of group>`\
+    \n•  **Function : **__Shows you the total information of the required chat.__"
+    \n\n  •  **Syntax : **`.get_id` or `.id`\
+    \n  •  **Function : **__if given input then shows id of that given chat/channel/user else if you reply to user then shows id of the replied user \
+    along with current chat id and if not replied to user or given input then just show id of the chat where you used the command__"
+    }
+)
