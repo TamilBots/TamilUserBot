2bfddcf015428e31f4bf338cd8f039b4f0c96162
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-04-02 22:13:32 +0530

    Update chatinfo.py

--- userbot/plugins/chatinfo.py
+++ userbot/plugins/chatinfo.py
@@ -20,7 +20,7 @@ from telethon.tl.types import MessageActionChannelMigrateFrom
 from telethon.utils import get_input_location, pack_bot_file_id
 
 from userbot.utils import admin_cmd
-from userbot import 
+from userbot import CMD_HELP
 from telethon.tl.types import (
     ChannelParticipantAdmin,
     ChannelParticipantCreator,
