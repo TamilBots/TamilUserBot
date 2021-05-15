04faee05a15aa4070ef1587cf1daf1e7245107b6
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-05-15 07:18:15 -0700

    Update tagall.py

--- userbot/plugins/tagall.py
+++ userbot/plugins/tagall.py
@@ -1,16 +1,40 @@
-""".tagall Plugin for @UniBorg"""
-from telethon import events
-from userbot.utils import admin_cmd
-from userbot import CMD_HELP
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2
 
-@borg.on(admin_cmd(pattern="tagall"))
+from telethon.tl.types import ChannelParticipantsAdmins
+
+from userbot.utils import admin_cmd
+
+
+
+@borg.on(admin_cmd(pattern=r"administrator", outgoing=True))
 async def _(event):
     if event.fwd_from:
         return
-    mentions = "@tagall"
+    mentions = "Administrators in the chat : "
+    chat = await event.get_input_chat()
+    async for x in borg.iter_participants(chat, filter=ChannelParticipantsAdmins):
+        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
+    reply_message = None
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        await reply_message.reply(mentions)
+    else:
+        await event.reply(mentions)
+    await event.delete()
+
+
+
+
+@borg.on(admin_cmd(pattern=r"tagall", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    mentions = "Hey there!"
     chat = await event.get_input_chat()
     async for x in borg.iter_participants(chat, 100):
-        mentions += f"[\u2063](tg://user?id={x.id})"
+        mentions += f" \n [{x.first_name}](tg://user?id={x.id})"
     await event.reply(mentions)
     await event.delete()
 
