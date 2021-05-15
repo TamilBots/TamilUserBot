4421cb023f1667dd41a6e9870ee1426783a70bb7
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-05-15 08:05:44 -0700

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -308,8 +308,8 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
 
 def paginate_help(page_number, loaded_plugins, prefix):
-    number_of_rows = 3
-    number_of_cols = 6  
+    number_of_rows = 8
+    number_of_cols = 2  
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
