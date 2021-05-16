5885140905f19c5338cee002aa6660bbe24bcdd9
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-05-16 21:53:20 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -351,10 +351,12 @@ def paginate_help(page_number, loaded_plugins, prefix):
         if not p.startswith("_"):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
-    modules = [custom.Button.inline(
-        "{} {} {}".format("ethuku", x, "ethuku"),
-        data="us_plugin_{}".format(x))
-        for x in helpable_plugins]
+    modules = [
+        custom.Button.inline(
+            "{} {} {}".format("ethuku", x, "ethuku"),data="us_plugin_{}".format(x)
+        )
+        for x in helpable_plugins
+    ]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
     if len(modules) % number_of_cols == 1:
         pairs.append((modules[-1],))
