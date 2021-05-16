2789ce5b289ad3995dff594c9faf6137293bda85
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-05-16 19:30:39 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -339,15 +339,16 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
 
 
 def paginate_help(page_number, loaded_plugins, prefix):
-    number_of_rows = 8
-    number_of_cols = 2  
+    number_of_rows = HLP_ROWS
+    number_of_cols = HLP_COLOUMNS
+    ethuku = CUSTM_HLP_EMOJ 
     helpable_plugins = []
     for p in loaded_plugins:
         if not p.startswith("_"):
             helpable_plugins.append(p)
     helpable_plugins = sorted(helpable_plugins)
     modules = [custom.Button.inline(
-        "{} {} {}".format("✯", x, "✯"),
+        "{} {} {}".format("ethuku", x, "ethuku"),
         data="us_plugin_{}".format(x))
         for x in helpable_plugins]
     pairs = list(zip(modules[::number_of_cols], modules[1::number_of_cols]))
