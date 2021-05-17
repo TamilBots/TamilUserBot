92e9633ad3b17906c031c670c22e727c31fc6c04
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-05-17 19:08:20 +0530

    Update Groupdata.py

--- userbot/plugins/Groupdata.py
+++ userbot/plugins/Groupdata.py
@@ -12,7 +12,8 @@ from telethon.tl.functions.messages import GetFullChatRequest, GetHistoryRequest
 from telethon.tl.types import MessageActionChannelMigrateFrom
 from telethon.utils import get_input_location, pack_bot_file_id
 
-from userbot.utils import admin_cmd, CMD_HELP
+from userbot import CMD_HELP
+from userbot.utils import admin_cmd
 from telethon.tl.types import (
     ChannelParticipantAdmin,
     ChannelParticipantCreator,
