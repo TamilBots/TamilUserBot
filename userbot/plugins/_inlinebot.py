ad02ca1aa74bda7d17d5918778019fdd0b4128d6
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-05-18 15:15:09 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -44,7 +44,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
             buttons = paginate_help(0, CMD_LIST, "helpme")
             result = builder.article(
                 "© TamilBot Help",
-                text="{}\n🔘𝐋𝐨𝐚𝐝𝐞𝐝 𝐏𝐥𝐮𝐠𝐢𝐧𝐬: {}".format(query, len(CMD_LIST)),
+                text="{}\n\n🔘𝐋𝐨𝐚𝐝𝐞𝐝 𝐏𝐥𝐮𝐠𝐢𝐧𝐬: {}".format(query, len(CMD_LIST)),
                 buttons=buttons,
                 link_preview=False,
             )
