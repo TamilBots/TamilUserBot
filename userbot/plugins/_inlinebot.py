5ff717b42505aa9675346784ef2e2ed0bc11c2a1
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-05-18 18:46:47 +0530

    Update _inlinebot.py

--- userbot/plugins/_inlinebot.py
+++ userbot/plugins/_inlinebot.py
@@ -98,7 +98,7 @@ if Var.TG_BOT_USER_NAME_BF_HER is not None and tgbot is not None:
                 ],
                 link_preview=False,
             )
-        await event.answer([result] if result else None)
+        await event.answer([result])
 
 
     @tgbot.on(
