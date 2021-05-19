ce62988fd4da8aee0f3c4ad53961f3aefea26561
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-05-19 21:22:55 +0530

    Update Afk.py

--- userbot/plugins/Afk.py
+++ userbot/plugins/Afk.py
@@ -7,7 +7,7 @@ from telethon.tl import functions, types
 from userbot import ALIVE_NAME, CMD_HELP
 from userbot.utils import admin_cmd
 from userbot import BOTLOG, BOTLOG_CHATID
-from userbot.manager.utils import edit_delete, edit_or_reply
+from userbot.manager.utils import edit_delete, edit_or_reply, media_type
 
 class AFK:
     def __init__(self):
