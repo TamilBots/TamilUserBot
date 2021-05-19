e69b17ac43f980ac55d5be929c0d3cf77ad8fea8
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-05-19 20:19:08 +0530

    Update Youtube.py

--- userbot/plugins/Youtube.py
+++ userbot/plugins/Youtube.py
@@ -10,7 +10,7 @@ import time
 from datetime import datetime
 from pathlib import Path
 from userbot.utils import progress
-from userbot import CMD_HELP, ALIVE_NAME
+from userbot import CMD_HELP, ALIVE_NAME, admin_cmd
 from telethon.errors.rpcerrorlist import YouBlockedUserError
 from telethon.tl.types import DocumentAttributeAudio
 from youtube_dl import YoutubeDL
