c9f6827a01850022bcece379185b0163c311234c
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-05-19 21:04:04 +0530

    Update Log_pms.py

--- userbot/plugins/Log_pms.py
+++ userbot/plugins/Log_pms.py
@@ -4,8 +4,8 @@ import asyncio
 from telethon import events
 
 from userbot import BOTLOG, BOTLOG_CHATID, LOGS
-from userbot.sql_helper import no_log_pms_sql
-from userbot.sql_helper.globals import addgvar, gvarstatus
+from userbot.plugins.sql_helper import no_log_pms_sql
+from userbot.plugins.sql_helper.globals import addgvar, gvarstatus
 from userbot.utils import admin_cmd
 from userbot.manager.utils import edit_delete, edit_or_reply
 
