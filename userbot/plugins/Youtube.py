996141670048f982cf8726e221941413642b6ca9
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-05-31 23:10:10 -0700

    Update Youtube.py

--- userbot/plugins/Youtube.py
+++ userbot/plugins/Youtube.py
@@ -31,7 +31,6 @@ parse_pre="html"
 
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "TamilUserBot"
 
-hmention = f"<a href = tg://user?id={USERID}>{DEFAULTUSER}</a>"
 from . import hmention, progress, ytsearch
 
 
