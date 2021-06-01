ff520963ffe0d70d78963f363df3b7a529ced48d
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-06-01 22:17:49 +0530

    Update tagnotify.py

--- userbot/plugins/tagnotify.py
+++ userbot/plugins/tagnotify.py
@@ -7,7 +7,7 @@ from userbot.uniborgConfig import Config
 if Config.PRIVATE_GROUP_ID:
     NEEDTOLOG = int(Config.PRIVATE_GROUP_ID)
     
-if Config.PRIVATE_GROUP_ID:
+if var.PRIVATE_GROUP_ID is not none:
 
     @borg.on(
         events.NewMessage(
@@ -17,43 +17,43 @@ if Config.PRIVATE_GROUP_ID:
         )
     )
     async def all_messages_catcher(e):
-        if NEEDTOLOG is not None
-            x = await borg.get_entity(e.sender_id)
-            if x.bot or x.verified:
-                return
-            y = await borg.get_entity(e.chat_id)
-            if y.username:
-                yy = f"[{get_display_name(y)}](https://t.me/{y.username})"
-            else:
-                yy = f"[{get_display_name(y)}](https://t.me/c/{y.id}/{e.id})"
-            xx = f"[{get_display_name(x)}](tg://user?id={x.id})"
-            msg = f"https://t.me/c/{y.id}/{e.id}"
+        await event.forward_to(Var.TG_BOT_USERNAME)
+        x = await ultroid_bot.get_entity(e.sender_id)
+        if x.bot or x.verified:
+            return
+        y = await ultroid_bot.get_entity(e.chat_id)
+        if y.username:
+            yy = f"[{get_display_name(y)}](https://t.me/{y.username})"
+        else:
+            yy = f"[{get_display_name(y)}](https://t.me/c/{y.id}/{e.id})"
+        xx = f"[{get_display_name(x)}](tg://user?id={x.id})"
+        msg = f"https://t.me/c/{y.id}/{e.id}"
+        if e.text:
+            cap = f"{xx} tagged you in {yy}\n\n```{e.text}```\nㅤ"
+        else:
+            cap = f"{xx} tagged you in {yy}"
+
+        btx = "📨 View Message"
+
+        try:
             if e.text:
-                cap = f"{xx} tagged you in {yy}\n\n```{e.text}```\nㅤ"
+                cap = get_string("tagnot_1").format(xx, yy, e.text, msg)
             else:
-                cap = f"{xx} tagged you in {yy}"
-
-            btx = "📨 View Message"
-
+                cap = get_string("tagnot_2").format(xx, yy, msg)
+            await asst.send_message(
+                NEEDTOLOG,
+                cap,
+                link_preview=False,
+                buttons=[[custom.Button.url(btx, msg)]],
+            )
+        except BaseException:
+            if e.text:
+                cap = get_string("tagnot_1").format(xx, yy, e.text, msg)
+            else:
+                cap = get_string("tagnot_2").format(xx, yy, msg)
             try:
-                if e.text:
-                    cap = get_string("tagnot_1").format(xx, yy, e.text, msg)
-                else:
-                    cap = get_string("tagnot_2").format(xx, yy, msg)
-                await borg.send_message(
-                    NEEDTOLOG,
-                    cap,
-                    link_preview=False,
-                    buttons=[[custom.Button.url(btx, msg)]],
-                )
+                await ultroid_bot.send_message(NEEDTOLOG, cap, link_preview=False)
             except BaseException:
-                if e.text:
-                    cap = get_string("tagnot_1").format(xx, yy, e.text, msg)
-                else:
-                    cap = get_string("tagnot_2").format(xx, yy, msg)
-                try:
-                    await borg.send_message(NEEDTOLOG, cap, link_preview=False)
-                except BaseException:
-                    pass
-            else:
-                return
+                pass
+    else: 
+        return
