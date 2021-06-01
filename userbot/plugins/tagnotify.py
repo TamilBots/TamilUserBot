a2620c8f6a2201d5ad09c4884ef5682e09f211b1
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-06-01 22:00:17 +0530

    Update tagnotify.py

--- userbot/plugins/tagnotify.py
+++ userbot/plugins/tagnotify.py
@@ -17,43 +17,43 @@ if Config.PRIVATE_GROUP_ID:
         )
     )
     async def all_messages_catcher(e):
-       if NEEDTOLOG is not None
-        x = await borg.get_entity(e.sender_id)
-        if x.bot or x.verified:
-            return
-        y = await borg.get_entity(e.chat_id)
-        if y.username:
-            yy = f"[{get_display_name(y)}](https://t.me/{y.username})"
-        else:
-            yy = f"[{get_display_name(y)}](https://t.me/c/{y.id}/{e.id})"
-        xx = f"[{get_display_name(x)}](tg://user?id={x.id})"
-        msg = f"https://t.me/c/{y.id}/{e.id}"
-        if e.text:
-            cap = f"{xx} tagged you in {yy}\n\n```{e.text}```\nㅤ"
-        else:
-            cap = f"{xx} tagged you in {yy}"
-
-        btx = "📨 View Message"
-
-        try:
-            if e.text:
-                cap = get_string("tagnot_1").format(xx, yy, e.text, msg)
+        if NEEDTOLOG is not None
+            x = await borg.get_entity(e.sender_id)
+            if x.bot or x.verified:
+                return
+            y = await borg.get_entity(e.chat_id)
+            if y.username:
+                yy = f"[{get_display_name(y)}](https://t.me/{y.username})"
             else:
-                cap = get_string("tagnot_2").format(xx, yy, msg)
-            await borg.send_message(
-                NEEDTOLOG,
-                cap,
-                link_preview=False,
-                buttons=[[custom.Button.url(btx, msg)]],
-            )
-        except BaseException:
+                yy = f"[{get_display_name(y)}](https://t.me/c/{y.id}/{e.id})"
+            xx = f"[{get_display_name(x)}](tg://user?id={x.id})"
+            msg = f"https://t.me/c/{y.id}/{e.id}"
             if e.text:
-                cap = get_string("tagnot_1").format(xx, yy, e.text, msg)
+                cap = f"{xx} tagged you in {yy}\n\n```{e.text}```\nㅤ"
             else:
-                cap = get_string("tagnot_2").format(xx, yy, msg)
+                cap = f"{xx} tagged you in {yy}"
+
+            btx = "📨 View Message"
+
             try:
-                await borg.send_message(NEEDTOLOG, cap, link_preview=False)
+                if e.text:
+                    cap = get_string("tagnot_1").format(xx, yy, e.text, msg)
+                else:
+                    cap = get_string("tagnot_2").format(xx, yy, msg)
+                await borg.send_message(
+                    NEEDTOLOG,
+                    cap,
+                    link_preview=False,
+                    buttons=[[custom.Button.url(btx, msg)]],
+                )
             except BaseException:
-                pass
-        else:
-            return
+                if e.text:
+                    cap = get_string("tagnot_1").format(xx, yy, e.text, msg)
+                else:
+                    cap = get_string("tagnot_2").format(xx, yy, msg)
+                try:
+                    await borg.send_message(NEEDTOLOG, cap, link_preview=False)
+                except BaseException:
+                    pass
+            else:
+                return
