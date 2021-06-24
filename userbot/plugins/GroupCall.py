0a36ed47dfcca90e6b47cec234a5e12fd2fda495
IMVETRI <68427115+IVETRI@users.noreply.github.com>
2021-06-24 07:47:44 -0700

    Update GroupCall.py

--- userbot/plugins/GroupCall.py
+++ userbot/plugins/GroupCall.py
@@ -15,56 +15,56 @@ def user_list(l, n):
         yield l[i : i + n]
 
 @register(pattern="^.startvc$", groups_only=True)
-async def apk(e):
-    chat = await e.get_chat()
+async def _(event):
+    chat = await event.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
     photo = None
 
     if not admin and not creator:
-        await e.edit("*You Are Not An Admin* 👮🏻‍♂️")
+        await event.edit("*You Are Not An Admin* 👮🏻‍♂️")
         return
     try:
-        await e.client(startvc(e.chat_id))
-        await e.edit("`Voice Call Started` 🤗")
+        await event.client(startvc(event.chat_id))
+        await event.edit("`Voice Call Started` 🤗")
     except Exception as ex:
-        await e.edit(f"Error : `{ex}`")
+        await event.edit(f"Error : `{ex}`")
 
 @register(outgoing=True, pattern="^.stopvc", groups_only=True)
-async def apk(e):
-    chat = await e.get_chat()
+async def _(event):
+    chat = await event.get_chat()
     admin = chat.admin_rights
     creator = chat.creator
     photo = None
 
     if not admin and not creator:
-        await e.edit("*You Are Not An Admin* 👮")
+        await event.edit("*You Are Not An Admin* 👮")
         return
     try:
-        await e.client(stopvc(await get_call(e)))
-        await e.edit("`The Group Call Was Stopped` 😥")
+        await event.client(stopvc(await get_call(event)))
+        await event.edit("`The Group Call Was Stopped` 😥")
     except Exception as ex:
-        await e.edit(f"Error : `{ex}`")
+        await event.edit(f"Error : `{ex}`")
 
 
 
 @register(outgoing=True, pattern="^.vctag", groups_only=True,)
-async def apk(e):
-    await e.edit("`Users are called by voice call ...` 😉")
+async def _(event):
+    await event.edit("`Users are called by voice call ...` 😉")
     users = []
     z = 0
-    async for x in e.client.iter_participants(e.chat_id):
+    async for x in event.client.iter_participants(e.chat_id):
         if not x.bot:
             users.append(x.id)
     hmm = list(user_list(users, 6))
     for p in hmm:
         try:
-            await e.client(invitetovc(call=await get_call(e), users=p))
+            await event.client(invitetovc(call=await get_call(event), users=p))
             z += 6
         except BaseException:
             pass
-    await e.edit(f"`{z} The User Was Called`")
+    await event.edit(f"`{z} The User Was Called`")
 
-CmdHelp('groupcall').add_command('startvc' , None, 'Səsli söhbət başladar').add_command('stopvc', None, 'Səsli söhbəti dayandırar').add_command('vctag', None, 'İnsanları səsli söhbətə dəvət edər').add()
+CmdHelp('groupcall').add_command('startvc' , None, 'Starts a voice conversation').add_command('stopvc', None, 'Stops voice conversation').add_command('vctag', None, 'Invites people to a voice conversation').add()
 
 
