655d6a8be15cf523caf279d66c6403909d68b2a4
Sathishzus <73419683+Sathishzus@users.noreply.github.com>
2021-06-30 11:25:02 +0530

    Create News.py

---
+++ userbot/plugins/News.py
@@ -0,0 +1,36 @@
+import requests
+
+from userbot.utils import admin_cmd
+from userbot.manager.utils import edit_or_reply
+from var import Var
+
+newslog = Var.NEWS_CHANNEL_ID
+
+
+@borg.on(admin_cmd("news (.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    if Var.NEWS_CHANNEL_ID is None:
+        await edit_or_reply(
+            event, "`Please ADD NEWS_CHANNEL_ID For This Module To Work`"
+        )
+        return
+    infintyvar = event.pattern_match.group(1)
+    main_url = f"https://inshortsapi.vercel.app/news?category={infintyvar}"
+    stuber = await edit_or_reply(
+        event,
+        f"Ok ! Fectching {infintyvar} From inshortsapi Server And Sending To News Channel",
+    )
+    await stuber.edit("All News Has Been Sucessfully Send To News Channel")
+    starknews = requests.get(main_url).json()
+    for item in starknews["data"]:
+        sedlyf = item["content"]
+        img = item["imageUrl"]
+        writter = item["author"]
+        dateis = item["date"]
+        readthis = item["readMoreUrl"]
+        titles = item["title"]
+        sed1 = img
+        sedm = f"**Title : {titles}** \n{sedlyf} \nDate : {dateis} \nAuthor : {writter} \nReadMore : {readthis}"
+        await borg.send_file(newslog, sed1, caption=sedm)
