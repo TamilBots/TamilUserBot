bd3821e025ee93823fbf961643596037deb384cd
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-07-22 13:49:16 +0530

    Update song.py

--- userbot/plugins/song.py
+++ userbot/plugins/song.py
@@ -1,37 +1,70 @@
 
-import glob
-import subprocess
+import os
+import requests
+import aiohttp
+import youtube_dl
+from pyrogram import filters, Client
+from youtube_search import YoutubeSearch
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, InputTextMessageContent
 
-from userbot.utils import admin_cmd
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60 ** i for i, x in enumerate(reversed(stringt.split(':'))))
 
+@borg.on_message(filters.command("song") & ~filters.edited)
+async def song(client, message):
 
-@borg.on(admin_cmd(pattern="sng ?(.*)"))
-async def _(event):
-    if event.fwd_from:
+    query = ''
+    for i in message.command[1:]:
+        query += ' ' + str(i)
+    print(query)
+    shed = message.reply("🔎 Finding the song...")
+    ydl_opts = {
+       "format": "bestaudio[ext=m4a]",
+       "geo-bypass": True,
+       "nocheckcertificate": True,
+       "outtmpl": "downloads/%(id)s.%(ext)s",
+       }
+    try:
+        results = YoutubeSearch(query, max_results=1).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        #print(results)
+        title = results[0]["title"][:40]       
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f'thumb{title}.jpg'
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, 'wb').write(thumb.content)
+
+        duration = results[0]["duration"]
+        url_suffix = results[0]["url_suffix"]
+        views = results[0]["views"]
+        channel = results[0]["channel"]
+    except Exception as e:
+        shed.edit(
+            "❌ Found Nothing.\n\nTry another keywork or maybe spell it properly."
+        )
+        print(str(e))
         return
-    await event.edit(
-        "**Getting Your Music it may take a few seconds to fetch the song from you tube and download it..**"
-    )
-    cmd = event.pattern_match.group(1)
-    cmnd = f"{cmd}"
-    reply_to_id = event.message.id
-    if event.reply_to_msg_id:
-        reply_to_id = event.reply_to_msg_id
-    subprocess.run(["spotdl", "-s", cmnd, "-q", "best"])
-    subprocess.run(
-        'for f in *.opus; do      mv -- "$f" "${f%.opus}.mp3"; done', shell=True
-    )
-    l = glob.glob("*.mp3")
-    loa = l[0]
-    await event.edit("`𝐘𝐞𝐚𝐡, 𝐈 𝐟𝐨𝐮𝐧𝐝 𝐭𝐡𝐞 𝐬𝐨𝐧𝐠..🎶`")
-    await event.client.send_file(
-        event.chat_id,
-        loa,
-        force_document=False,
-        allow_cache=False,
-        supports_streaming=True,
-        caption= f'🥰 𝐮𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐬𝐮𝐜𝐜𝐞𝐬𝐬𝐟𝐮𝐥𝐥𝐲\n🎬 𝐒𝐨𝐮𝐫𝐜𝐞 : [Youtube](https://youtu.be/3pN0W4KzzNY)\n\n💌 𝐁𝐲 : @TamilUserBot',
-        reply_to=reply_to_id,
-    )
-    subprocess.run("rm -rf *.mp3", shell=True)
+    shed.edit("📥 Downloading...")
+    try:
+        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
+        rep = '🥰 𝐮𝐩𝐥𝐨𝐚𝐝𝐞𝐝 𝐁𝐲 @TamiliniBot'
+        secmul, dur, dur_arr = 1, 0, duration.split(':')
+        for i in range(len(dur_arr)-1, -1, -1):
+            dur += (int(dur_arr[i]) * secmul)
+            secmul *= 60
+        shed.edit("📤 Uploading...")
+        s = message.reply_audio(audio_file, caption=rep, thumb=thumb_name, parse_mode='md', title=title, duration=dur, performer=channel)
+        shed.delete()
+    except Exception as e:
+        shed.edit("❌ Error")
+        print(e)
 
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as e:
+        print(e)
