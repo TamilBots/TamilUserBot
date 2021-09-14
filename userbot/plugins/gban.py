d3ac6b2b1f6ed80fcf99deb26584f6ea8985ac19
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-09-14 17:15:35 +0530

    Create gban.py

---
+++ userbot/plugins/gban.py
@@ -0,0 +1,194 @@
+import asyncio
+from datetime import datetime
+
+from telethon.errors import BadRequestError
+from telethon.tl.functions.channels import EditBannedRequest
+from telethon.tl.functions.users import GetFullUserRequest
+from telethon.tl.types import ChatBannedRights
+from telethon.utils import get_display_name
+
+
+from userbot.manager.utils import edit_delete, edit_or_reply
+from userbot.plugins sql_helper import gban_sql_helper as gban_sql
+from userbot import BOTLOG, BOTLOG_CHATID
+
+
+async def admin_groups(catub):
+    catgroups = []
+    async for dialog in catub.iter_dialogs():
+        entity = dialog.entity
+        if (
+            isinstance(entity, Channel)
+            and entity.megagroup
+            and (entity.creator or entity.admin_rights)
+        ):
+            catgroups.append(entity.id)
+    return catgroups
+
+
+BANNED_RIGHTS = ChatBannedRights(
+    until_date=None,
+    view_messages=True,
+    send_messages=True,
+    send_media=True,
+    send_stickers=True,
+    send_gifs=True,
+    send_games=True,
+    send_inline=True,
+    embed_links=True,
+)
+
+UNBAN_RIGHTS = ChatBannedRights(
+    until_date=None,
+    send_messages=None,
+    send_media=None,
+    send_stickers=None,
+    send_gifs=None,
+    send_games=None,
+    send_inline=None,
+    embed_links=None,
+)
+
+
+@borg.on(admin_cmd("gban(?: |$)(.*)"))
+async def catgban(event):  # sourcery no-metrics
+    "To ban user in every group where you are admin."
+    cate = await edit_or_reply(event, "`gbanning.......`")
+    start = datetime.now()
+    user, reason = await get_user_from_event(event, cate)
+    if not user:
+        return
+    if user.id == catub.uid:
+        return await edit_delete(cate, "`why would I ban myself`")
+    if gban_sql.is_gbanned(user.id):
+        await cate.edit(
+            f"`the `[user](tg://user?id={user.id})` is already in gbanned list any way checking again`"
+        )
+    else:
+        gban_sql.catgban(user.id, reason)
+    san = await admin_groups(event.client)
+    count = 0
+    sandy = len(san)
+    if sandy == 0:
+        return await edit_delete(cate, "`you are not admin of atleast one group` ")
+    await cate.edit(
+        f"`initiating gban of the `[user](tg://user?id={user.id}) `in {len(san)} groups`"
+    )
+    for i in range(sandy):
+        try:
+            await event.client(EditBannedRequest(san[i], user.id, BANNED_RIGHTS))
+            await asyncio.sleep(0.5)
+            count += 1
+        except BadRequestError:
+            achat = await event.client.get_entity(san[i])
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"`You don't have required permission in :`\n**Chat :** {get_display_name(achat)}(`{achat.id}`)\n`For banning here`",
+            )
+    end = datetime.now()
+    cattaken = (end - start).seconds
+    if reason:
+        await cate.edit(
+            f"[{user.first_name}](tg://user?id={user.id}) `was gbanned in {count} groups in {cattaken} seconds`!!\n**Reason :** `{reason}`"
+        )
+    else:
+        await cate.edit(
+            f"[{user.first_name}](tg://user?id={user.id}) `was gbanned in {count} groups in {cattaken} seconds`!!"
+        )
+    if BOTLOG and count != 0:
+        reply = await event.get_reply_message()
+        if reason:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"#GBAN\
+                \nGlobal Ban\
+                \n**User : **[{user.first_name}](tg://user?id={user.id})\
+                \n**ID : **`{user.id}`\
+                \n**Reason :** `{reason}`\
+                \n__Banned in {count} groups__\
+                \n**Time taken : **`{cattaken} seconds`",
+            )
+        else:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"#GBAN\
+                \nGlobal Ban\
+                \n**User : **[{user.first_name}](tg://user?id={user.id})\
+                \n**ID : **`{user.id}`\
+                \n__Banned in {count} groups__\
+                \n**Time taken : **`{cattaken} seconds`",
+            )
+        try:
+            if reply:
+                await reply.forward_to(BOTLOG_CHATID)
+                await reply.delete()
+        except BadRequestError:
+            pass
+
+
+@borg.on(admin_cmd("ungban(?: |$)(.*)"))
+async def catgban(event):
+    "To unban the person from every group where you are admin."
+    cate = await edit_or_reply(event, "`ungbanning.....`")
+    start = datetime.now()
+    user, reason = await get_user_from_event(event, cate)
+    if not user:
+        return
+    if gban_sql.is_gbanned(user.id):
+        gban_sql.catungban(user.id)
+    else:
+        return await edit_delete(
+            cate, f"the [user](tg://user?id={user.id}) `is not in your gbanned list`"
+        )
+    san = await admin_groups(event.client)
+    count = 0
+    sandy = len(san)
+    if sandy == 0:
+        return await edit_delete(cate, "`you are not even admin of atleast one group `")
+    await cate.edit(
+        f"initiating ungban of the [user](tg://user?id={user.id}) in `{len(san)}` groups"
+    )
+    for i in range(sandy):
+        try:
+            await event.client(EditBannedRequest(san[i], user.id, UNBAN_RIGHTS))
+            await asyncio.sleep(0.5)
+            count += 1
+        except BadRequestError:
+            achat = await event.client.get_entity(san[i])
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"`You don't have required permission in :`\n**Chat :** {get_display_name(achat)}(`{achat.id}`)\n`For Unbanning here`",
+            )
+    end = datetime.now()
+    cattaken = (end - start).seconds
+    if reason:
+        await cate.edit(
+            f"[{user.first_name}](tg://user?id={user.id}`) was ungbanned in {count} groups in {cattaken} seconds`!!\n**Reason :** `{reason}`"
+        )
+    else:
+        await cate.edit(
+            f"[{user.first_name}](tg://user?id={user.id}) `was ungbanned in {count} groups in {cattaken} seconds`!!"
+        )
+
+    if BOTLOG and count != 0:
+        if reason:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"#UNGBAN\
+                \nGlobal Unban\
+                \n**User : **[{user.first_name}](tg://user?id={user.id})\
+                \n**ID : **`{user.id}`\
+                \n**Reason :** `{reason}`\
+                \n__Unbanned in {count} groups__\
+                \n**Time taken : **`{cattaken} seconds`",
+            )
+        else:
+            await event.client.send_message(
+                BOTLOG_CHATID,
+                f"#UNGBAN\
+                \nGlobal Unban\
+                \n**User : **[{user.first_name}](tg://user?id={user.id})\
+                \n**ID : **`{user.id}`\
+                \n__Unbanned in {count} groups__\
+                \n**Time taken : **`{cattaken} seconds`",
+            )
