6e61311ef4e1684f09388ebfc5dee189772d60e7
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-09-14 18:25:51 +0530

    Update gban.py

--- userbot/plugins/gban.py
+++ userbot/plugins/gban.py
@@ -1,5 +1,6 @@
 import asyncio
 from datetime import datetime
+import logging
 
 from telethon.tl.types import MessageEntityMentionName
 from telethon.errors import BadRequestError
@@ -14,6 +15,8 @@ from userbot.plugins.sql_helper import gban_sql_helper as gban_sql
 from userbot import BOTLOG, BOTLOG_CHATID
 from userbot.utils import admin_cmd
 
+LOGS = logging.getLogger(__name__)
+
 async def admin_groups(catub):
     catgroups = []
     async for dialog in catub.iter_dialogs():
@@ -26,35 +29,64 @@ async def admin_groups(catub):
             catgroups.append(entity.id)
     return catgroups
 
-async def get_user_from_event(event):
-    """ Get the user from argument or replied message. """
-    args = event.pattern_match.group(1).split(":", 1)
+async def get_user_from_event(
+    event, catevent=None, secondgroup=None, nogroup=False, noedits=False
+):  # sourcery no-metrics
+    if catevent is None:
+        catevent = event
+    if nogroup is False:
+        if secondgroup:
+            args = event.pattern_match.group(2).split(" ", 1)
+        else:
+            args = event.pattern_match.group(1).split(" ", 1)
     extra = None
-    if event.reply_to_msg_id and len(args) != 2:
-        previous_message = await event.get_reply_message()
-        user_obj = await event.client.get_entity(previous_message.sender_id)
-        extra = event.pattern_match.group(1)
-    elif len(args[0]) > 0:
-        user = args[0]
-        if len(args) == 2:
-            extra = args[1]
-        if user.isnumeric():
-            user = int(user)
-        if not user:
-            await event.edit("`Pass the user's username, id or reply!`")
-            return
-        if event.message.entities:
-            probable_user_mention_entity = event.message.entities[0]
-            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
-                user_id = probable_user_mention_entity.user_id
-                user_obj = await event.client.get_entity(user_id)
-                return user_obj
-        try:
-            user_obj = await event.client.get_entity(user)
-        except (TypeError, ValueError) as err:
-            await event.edit(str(err))
-            return None
-    return user_obj, extra
+    try:
+        if args:
+            user = args[0]
+            if len(args) > 1:
+                extra = "".join(args[1:])
+            if user.isnumeric() or (user.startswith("-") and user[1:].isnumeric()):
+                user = int(user)
+            if event.message.entities:
+                probable_user_mention_entity = event.message.entities[0]
+                if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                    user_id = probable_user_mention_entity.user_id
+                    user_obj = await event.client.get_entity(user_id)
+                    return user_obj, extra
+            if isinstance(user, int) or user.startswith("@"):
+                user_obj = await event.client.get_entity(user)
+                return user_obj, extra
+    except Exception as e:
+        LOGS.error(str(e))
+    try:
+        if nogroup is False:
+            if secondgroup:
+                extra = event.pattern_match.group(2)
+            else:
+                extra = event.pattern_match.group(1)
+        if event.is_private:
+            user_obj = await event.get_chat()
+            return user_obj, extra
+        if event.reply_to_msg_id:
+            previous_message = await event.get_reply_message()
+            if previous_message.from_id is None:
+                if not noedits:
+                    await edit_delete(catevent, "`Well that's an anonymous admin !`")
+                return None, None
+            user_obj = await event.client.get_entity(previous_message.sender_id)
+            return user_obj, extra
+        if not args:
+            if not noedits:
+                await edit_delete(
+                    catevent, "`Pass the user's username, id or reply!`", 5
+                )
+            return None, None
+    except Exception as e:
+        LOGS.error(str(e))
+    if not noedits:
+        await edit_delete(catevent, "__Couldn't fetch user to proceed further.__")
+    return None, None
+
 
 BANNED_RIGHTS = ChatBannedRights(
     until_date=None,
