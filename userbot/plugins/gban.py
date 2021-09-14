5a97c9dafaf28735790a8168367e5e9badc130a5
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-09-14 17:36:13 +0530

    Update gban.py

--- userbot/plugins/gban.py
+++ userbot/plugins/gban.py
@@ -10,9 +10,9 @@ from telethon.utils import get_display_name
 
 from userbot.manager.utils import edit_delete, edit_or_reply
 from userbot.plugins.sql_helper import gban_sql_helper as gban_sql
-from userbot import BOTLOG, BOTLOG_CHATID, get_user_from_event
+from userbot import BOTLOG, BOTLOG_CHATID
 from userbot.utils import admin_cmd
-
+from userbot.plugins import get_user_from_event
 async def admin_groups(catub):
     catgroups = []
     async for dialog in catub.iter_dialogs():
