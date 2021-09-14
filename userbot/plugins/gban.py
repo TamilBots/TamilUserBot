0bc8e2f9fa2c7758ccde16f7f3a1004253ac0ea1
Tamil Bots <81024661+TamilBots@users.noreply.github.com>
2021-09-14 18:13:08 +0530

    Update gban.py

--- userbot/plugins/gban.py
+++ userbot/plugins/gban.py
@@ -1,6 +1,7 @@
 import asyncio
 from datetime import datetime
 
+from telethon.tl.types import MessageEntityMentionName
 from telethon.errors import BadRequestError
 from telethon.tl.functions.channels import EditBannedRequest
 from telethon.tl.functions.users import GetFullUserRequest
@@ -12,7 +13,7 @@ from userbot.manager.utils import edit_delete, edit_or_reply
 from userbot.plugins.sql_helper import gban_sql_helper as gban_sql
 from userbot import BOTLOG, BOTLOG_CHATID
 from userbot.utils import admin_cmd
-from userbot.plugins.info.py import get_user_from_event
+
 async def admin_groups(catub):
     catgroups = []
     async for dialog in catub.iter_dialogs():
@@ -25,6 +26,35 @@ async def admin_groups(catub):
             catgroups.append(entity.id)
     return catgroups
 
+async def get_user_from_event(event):
+    """ Get the user from argument or replied message. """
+    args = event.pattern_match.group(1).split(":", 1)
+    extra = None
+    if event.reply_to_msg_id and len(args) != 2:
+        previous_message = await event.get_reply_message()
+        user_obj = await event.client.get_entity(previous_message.sender_id)
+        extra = event.pattern_match.group(1)
+    elif len(args[0]) > 0:
+        user = args[0]
+        if len(args) == 2:
+            extra = args[1]
+        if user.isnumeric():
+            user = int(user)
+        if not user:
+            await event.edit("`Pass the user's username, id or reply!`")
+            return
+        if event.message.entities:
+            probable_user_mention_entity = event.message.entities[0]
+            if isinstance(probable_user_mention_entity, MessageEntityMentionName):
+                user_id = probable_user_mention_entity.user_id
+                user_obj = await event.client.get_entity(user_id)
+                return user_obj
+        try:
+            user_obj = await event.client.get_entity(user)
+        except (TypeError, ValueError) as err:
+            await event.edit(str(err))
+            return None
+    return user_obj, extra
 
 BANNED_RIGHTS = ChatBannedRights(
     until_date=None,
