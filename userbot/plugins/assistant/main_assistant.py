d89d022ed14940b7d1e84d84cf2747e4253aca6e
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-09-19 21:29:00 +0530

    Update main_assistant.py

--- userbot/plugins/assistant/main_assistant.py
+++ userbot/plugins/assistant/main_assistant.py
@@ -117,7 +117,7 @@ async def ddeploy(event):
         await tgbot.send_message(
             event.chat_id,
             message=f"𝐡𝐞𝐲 {firstname} ! 𝐜𝐥𝐢𝐜𝐤 𝐛𝐞𝐥𝐨𝐰 𝐛𝐮𝐭𝐭𝐨𝐧𝐬 𝐭𝐨 𝐠𝐞𝐭 𝐭𝐮𝐭𝐨𝐫𝐢𝐚𝐥 𝐯𝐢𝐝𝐞𝐨𝐬. \n\n𝐈𝐟 𝐲𝐨𝐮 𝐰𝐚𝐧𝐭 𝐦𝐨𝐫𝐞 𝐚𝐬𝐬𝐢𝐬𝐭𝐚𝐧𝐜𝐞 𝐣𝐮𝐬𝐭 𝐚𝐬𝐤 𝐡𝐞𝐫𝐞 @TamilSupport \n\n𝐏𝐨𝐰𝐞𝐫𝐞𝐝 𝐁𝐲 [Tamil Userbot](t.me/TamilBots) " 
-          buttons=[
+            buttons=[
                 [Button.url("ᴅᴇᴘʟᴏʏ ᴛᴏ ʜᴇʀᴏᴋᴜ", "https://youtu.be/3PgBiFgfx34")],
                 [Button.url("ᴅᴇᴘʟᴏʏ ᴛᴏ ʀᴀɪʟᴡᴀʏ", "https://github.com/TamilBotst")],
                 [custom.Button.inline("ʜᴏᴍᴇ", data="backme")],
