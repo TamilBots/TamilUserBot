9155e23467798cbcf5125ee86f3e2f47ca9c5d76
Saravanakrish <68471536+ImSaravanakrish@users.noreply.github.com>
2021-10-01 21:53:37 +0530

    Update fed.py

--- userbot/plugins/fed.py
+++ userbot/plugins/fed.py
@@ -185,6 +185,112 @@ async def _(event):
         )
     await event.delete()
 
+
+import os
+
+from telethon.errors import ChatAdminRequiredError
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.users import GetFullUserRequest
+
+from ..utils import admin_cmd
+from . import ALIVE_NAME
+
+naam = str(ALIVE_NAME)
+
+bots = "@MissRose_bot"
+
+BOTLOG_CHATID = Config.PRIVATE_GROUP_BOT_API_ID
+
+G_BAN_LOGGER_GROUP = os.environ.get("G_BAN_LOGGER_GROUP", None)
+if G_BAN_LOGGER_GROUP:
+    G_BAN_LOGGER_GROUP = int(G_BAN_LOGGER_GROUP)
+
+
+import asyncio
+
+#  (c)2020 Telebot
+#
+# You may not use this plugin without proper authorship and consent from @TelebotSupport
+#
+import os
+
+PRIVATE_GROUP_ID = os.environ.get("PRIVATE_GROUP_ID", None)
+if FBAN_GROUP_ID:
+    PRIVATE_GROUP_ID = int(PRIVATE_GROUP_ID)
+
+@bot.on(admin_cmd("superunfban ?(.*)"))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("Starting a Mass-UnFedBan...")
+    if event.reply_to_msg_id:
+        previous_message = await event.get_reply_message()
+        FBAN = previous_message.sender_id
+    else:
+        FBAN = event.pattern_match.group(1)
+
+    if PRIVATE_GROUP_ID:
+        chat = PRIVATE_GROUP_ID D
+    else:
+        chat = await event.get_chat()
+    fedList = []
+    for a in range(3):
+        async with event.client.conversation("@MissRose_bot") as bot_conv:
+            await bot_conv.send_message("/start")
+            await bot_conv.send_message("/myfeds")
+            response = await bot_conv.get_response()
+            if "make a file" in response.text:
+                await asyncio.sleep(1)
+                await response.click(0)
+                fedfile = await bot_conv.get_response()
+                if fedfile.media:
+                    downloaded_file_name = await event.client.download_media(
+                        fedfile, "fedlist"
+                    )
+                    file = open(downloaded_file_name, "r")
+                    lines = file.readlines()
+                    for line in lines:
+                        fedList.append(line[:36])
+                else:
+                    return
+                if len(fedList) == 0:
+                    await event.edit(f"Something went wrong. Retrying ({a+1}/3)...")
+                else:
+                    break
+    else:
+        await event.edit(f"Error")
+    if "You can only use fed commands once every 5 minutes" in response.text:
+        await event.edit("Try again after 5 mins.")
+        return
+    In = False
+    tempFedId = ""
+    for x in response.text:
+        if x == "`":
+            if In:
+                In = False
+                fedList.append(tempFedId)
+                tempFedId = ""
+            else:
+                In = True
+
+        elif In:
+            tempFedId += x
+
+    await event.edit(f"UnFbaning in {len(fedList)} feds.")
+    try:
+        await event.client.send_message(chat, f"/start")
+    except:
+        await event.edit("PRIVATE_GROUP_ID is incorrect.")
+        return
+    await asyncio.sleep(3)
+    for fed in fedList:
+        await event.client.send_message(chat, f"/joinfed {fed}")
+        await asyncio.sleep(3)
+        await event.client.send_message(chat, f"/unfban {FBAN}")
+        await asyncio.sleep(3)
+    await event.edit(f"SuperUnFBan Completed. Affected {len(fedList)} feds.")
+
+
 CMD_HELP.update(
     {
         "fed":
@@ -215,4 +321,6 @@ CMD_HELP.update(
 """
     }
 )
+
+
 
